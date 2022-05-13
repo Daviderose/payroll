c0218112acd687fc30331f424828a1e8ab69af38
David Rose <daviderose526@gmail.com>
2022-05-13 12:24:20 -0500

    refactored Employee entity to utilize first and last name instead of name.

--- src/main/java/com/example/payroll/Employee.java
+++ src/main/java/com/example/payroll/Employee.java
@@ -9,13 +9,15 @@ import java.util.Objects;
 public class Employee {
 
     private @Id @GeneratedValue Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String role;
 
     public Employee() {}
 
-    public Employee(String name, String role) {
-        this.name = name;
+    public Employee(String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
     }
 
@@ -28,11 +30,13 @@ public class Employee {
     }
 
     public String getName() {
-        return name;
+        return this.firstName + " " + this.lastName;
     }
 
     public void setName(String name) {
-        this.name = name;
+        String[] parts = name.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
     }
 
     public String getRole() {
@@ -51,20 +55,22 @@ public class Employee {
             return false;
         Employee employee = (Employee) o;
         return Objects.equals(this.id, employee.id) &&
-                Objects.equals(this.name, employee.name) &&
+                Objects.equals(this.firstName, employee.firstName) &&
+                Objects.equals(this.lastName, employee.lastName) &&
                 Objects.equals(this.role, employee.role);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, role);
+        return Objects.hash(id, firstName, lastName, role);
     }
 
     @Override
     public String toString() {
         return "Employee{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
                 ", role='" + role + '\'' +
                 '}';
     }
--- src/main/java/com/example/payroll/LoadDatabase.java
+++ src/main/java/com/example/payroll/LoadDatabase.java
@@ -15,9 +15,9 @@ public class LoadDatabase {
     CommandLineRunner initDatabase(EmployeeRepository repository) {
 
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burgler")));
-            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
-            log.info("Preloading " + repository.save(new Employee("Gandolf Graymane", "wizard")));
+            log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burgler")));
+            log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
+            log.info("Preloading " + repository.save(new Employee("Gandolf", "Graymane", "wizard")));
         };
     }
 }
